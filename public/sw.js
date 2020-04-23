c09f10f7fc61a5ffa7befc02c488d76f43463eba
irfahadi <irfanrsbi@gmail.com>
2020-04-23 16:15:29 +0700

    first

--- public/sw.js
+++ public/sw.js
@@ -8,10 +8,10 @@ self.addEventListener('install', function(){
 			'/img/BI.jpg',
 			'https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css',
 			'https://use.fontawesome.com/releases/v5.3.1/js/all.js',
-			'https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css',
-			'https://code.jquery.com/jquery-1.12.4.js',
+			// 'https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css',
+			// 'https://code.jquery.com/jquery-1.12.4.js',
 			'https://momentjs.com/downloads/moment.js',
-			'https://code.jquery.com/ui/1.12.1/jquery-ui.js'
+			// 'https://code.jquery.com/ui/1.12.1/jquery-ui.js'
 			]);
 		})
 	// );
